d0656a98d8244e72afdc894beb190364bd6415fe
odohpraise <odohpraise36@gmail.com>
2026-05-24 04:26:10 +0100

    css style improved

--- src/components/JobLists.tsx
+++ src/components/JobLists.tsx
@@ -4,25 +4,27 @@ import { jobs } from '../data/job'
 function JobLists() {
     return (
         <div className="container mt-4 mb-4">
-            <h1>JOBBERMAN</h1>
+            <h1 className='sticky-top'>JOBBERMAN</h1>
+            <p>Welcome to JOBBERMAN, your one-stop destination for finding the best job opportunities!</p>
 
-            {jobs.length === 0 ? (
+            {jobs.length === 0 ?
                 <p>No jobs available at the moment.</p>
-            ) : (
-                <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-                    {jobs.map((job) => (
-                        <div key={job.id}>
-                            <JobCard
-                                title={job.title}
-                                company={job.company}
-                                location={job.location}
-                                salary={job.salary}
-                                description={job.description}
-                            />
-                        </div>
-                    ))}
-                </div>
-            )}
+                : (
+
+                    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+                        {jobs.map((job) => (
+                            <div key={job.id}>
+                                <JobCard
+                                    title={job.title}
+                                    company={job.company}
+                                    location={job.location}
+                                    salary={job.salary}
+                                    description={job.description}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                )}
 
         </div>
     )
